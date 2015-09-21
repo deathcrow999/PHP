bbbcd94d7caedf94e0c31699c3539d53d80d7b84
deathcrow999 <deathcrow4561@gmail.com>
2015-09-21 00:22:35 -0400

    Update Class_Name.php

--- ContactProgram/Class_Name.php
+++ ContactProgram/Class_Name.php
@@ -1,3 +1,23 @@
 <?php
 
+
+class person{
+
+
+ public var $name;
+ 
+
+ public function setName($newName){
+  $this->name = $newName;
+ }
+
+ public function getName(){
+  return $this->name;
+ }
+
+}
+
+
+
+
 ?>
